5f7d3695ba5f009823680d019cd159942ecfd0a0
Sakamoto2004 <phbtoan9185@gmail.com>
2024-10-20 12:07:31 +0700

    Simplisfy the Engine

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -3,17 +3,17 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using Models.Model;
-using Helper;
 
 namespace Engine;
 
 public class Engine : Game
 {
-    private Witch _blueWitch; 
+//    private Witch _blueWitch; 
     GraphicsDeviceManager _graphics;
     SpriteBatch _spriteBatch;
     private Viewport _viewport;
 
+    private Knight _knight;
 
     public Engine()
     {
@@ -25,20 +25,29 @@ public class Engine : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        _blueWitch = new Witch(){Speed = 5};
+//        _blueWitch = new Witch(){Speed = 5};
+        _knight = new Knight(){ Speed = 5 };
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _blueWitch.Load(Content);
-        if(_blueWitch.SourceRectangle == null)
+//        _blueWitch.Load(Content);
+//        if(_blueWitch.SourceRectangle == null)
+//            throw new Exception("Unable to initialize source rectangle");
+//        if( _blueWitch.Textures == null )
+//            throw new Exception("Texture is null");
+
+        _knight.Load(Content);
+        if(_knight.SourceRectangle == null)
             throw new Exception("Unable to initialize source rectangle");
-        if( _blueWitch.Texture == null )
+        if( _knight.Textures == null )
             throw new Exception("Texture is null");
+
         _viewport = _graphics.GraphicsDevice.Viewport;
-        _blueWitch.Position = new Vector2(_viewport.Width / 2, _viewport.Height /2 );
+//        _blueWitch.Position = new Vector2(_viewport.Width / 2, _viewport.Height /2 );
+        _knight.Position = new Vector2(_viewport.Width / 2, _viewport.Height /2 );
 
         // TODO: use this.Content to load your game content here
     }
@@ -47,10 +56,9 @@ public class Engine : Game
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
         // TODO: Add your update logic here
         float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
-        _blueWitch.UpdateFrame(elapsed);
+        _knight.Control( elapsed );
 
         base.Update(gameTime);
     }
@@ -58,11 +66,9 @@ public class Engine : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Black);
-
-
         // TODO: Add your drawing code here
-        _spriteBatch.Begin();
-        _blueWitch.Draw(_spriteBatch);
+        _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+        _knight.Draw(_spriteBatch);
         _spriteBatch.End();
         base.Draw(gameTime);
     }
