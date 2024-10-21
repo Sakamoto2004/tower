782ae256e8c4f3a7c6664cd79baa4c2d3e05c5f5
Sakamoto2004 <phbtoan9185@gmail.com>
2024-10-21 16:47:48 +0700

    Fixed hovering delay

--- Helper/Objects/PhysicEngine.cs
+++ Helper/Objects/PhysicEngine.cs
@@ -6,6 +6,8 @@ public static class PhysicEngine{
     public static int SpeedCalculator(float acceleration, int speed, float elapsed){
         time += elapsed;
         float speedChange = time * acceleration;
+        if( (int) speedChange == 0 ) 
+            speedChange = 1;
         return (int) (speed + speedChange);
     }
 
